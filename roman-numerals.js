af54da450f7ac8a02692d464a788af36cac30c24
Matthew Morgan <matt@dulcetsoftware.com>
2016-03-17 19:56:11 -0400

    Refactor for better variable names

--- roman-numerals.js
+++ roman-numerals.js
@@ -1,14 +1,14 @@
 const DIGITS = new Map([[1000, 'M'], [900, 'CM'], [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']]);
 
 export default () => ({
-  fromDecimal: (decVal = Number(n)) => {
-    let result = '';
-    DIGITS.forEach((roman, decimal) => {
-      while (decVal >= decimal) {
-        result += roman;
-        decVal -= decimal;
+  fromDecimal: (decimalInput = Number(n)) => {
+    let romanResult = '';
+    DIGITS.forEach((romanSymbol, decimalValue) => {
+      while (decimalInput >= decimalValue) {
+        romanResult += romanSymbol;
+        decimalInput -= decimalValue;
       }
     });
-    return result;
+    return romanResult;
   }
 })
