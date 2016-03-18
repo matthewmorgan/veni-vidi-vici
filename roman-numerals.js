c9edf26ff70a6a0832c5ba366b51119cf763bbbb
Matthew Morgan <matt@dulcetsoftware.com>
2016-03-17 21:26:20 -0400

    Refactor to remove map duplication

--- roman-numerals.js
+++ roman-numerals.js
@@ -1,7 +1,5 @@
 const DIGITS = new Map([[1000, 'M'], [900, 'CM'], [100, 'C'], [90, 'XC'], [50, 'L'],
                         [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']]);
-const SYMBOLS = new Map();
-DIGITS.forEach((romanSymbol, decimalValue) => SYMBOLS.set(romanSymbol, decimalValue));
 
 export default class RomanNumerals {
 
@@ -23,7 +21,7 @@ export default class RomanNumerals {
     let romanDigits = n.split('');
 
     while (romanDigits.length > 0) {
-      SYMBOLS.forEach((decimalValue, romanSymbol) => {
+      DIGITS.forEach((romanSymbol, decimalValue) => {
         if (romanDigits.slice(0, romanSymbol.length).join('') === romanSymbol) {
           decimalResult += decimalValue;
           romanDigits.splice(0, romanSymbol.length);
